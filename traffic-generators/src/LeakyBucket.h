4ac27e21585fc7b8f58862ba03d5887a607a3687
intosoft <tomasz.marciniak@gmail.com>
2013-12-13 22:29:52 +0100

    Leaky bucket

---
+++ traffic-generators/src/LeakyBucket.h
@@ -0,0 +1,66 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef LEAKYBUCKET_H_
+#define LEAKYBUCKET_H_
+
+#include <stdlib.h>
+#include <list>
+#include <omnetpp.h>
+#include "Packet_m.h"
+
+/**
+ * Leaky Bucket
+ * @author Tomasz Obszarny
+ * @date 2013
+ * @copyright GNU Public License.
+ */
+class LeakyBucket : public cSimpleModule
+{
+	public:
+		LeakyBucket();
+		virtual ~LeakyBucket();
+
+	protected:
+		virtual void initialize();
+		virtual void handleMessage(cMessage* msg);
+		virtual void finish();
+		void addPacketToQueue(Packet* packet);
+		Packet* pickupNextPacketFromQueues();
+		Packet* pickupPacketFromQueue(int gateId);
+		bool isQueueForGateExistent(int gateId);
+		bool isQueueSizeExceeded(int gateId);
+		bool isQueueSizeExceeded(std::list<Packet*>* list);
+		bool isOutAttachedWithChannel();
+		std::list<Packet*>* getPacketListForGate(int gateId);
+		void rotateIndex();
+
+		std::map<int, std::list<Packet*>*>*  packetQueueMap;
+		unsigned int queueRotatorIndex;
+		cMessage* internalDispatchingMessage;
+		int inputChannelId[];
+
+		//Parameters
+		unsigned int queueSizeLimit;
+		simtime_t delay;
+
+
+		//Infrastructure
+		cGate* out;
+//		cGate* in[];
+
+};
+
+#endif /* LEAKYBUCKET_H_ */
